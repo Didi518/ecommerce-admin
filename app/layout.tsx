0950b2681bd6092ce42f1a243811f41488551d36
Didi518 <94155950+Didi518@users.noreply.github.com>
2023-07-20 11:04:58 +0200

    prisma sql :wrench:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
-import './globals.css';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+
 import { frFR } from '@clerk/localizations';
+import { ModalProvider } from '@/providers/modal-provider';
+import { ToasterProvider } from '@/providers/toast-provider';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Panneau Admin',
   description: 'Panneau Admin',
 };
@@ -19,7 +22,11 @@ export default function RootLayout({
   return (
     <ClerkProvider localization={frFR}>
       <html lang="fr">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
